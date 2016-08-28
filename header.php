73743f44c07c7705787ece3c7274da6243dae58f
lcdonaldson <lcdbacanas@gmail.com>
2016-08-28 00:41:15 -0500

    Update header.php

--- header.php
+++ header.php
@@ -1,12 +1,11 @@
 
-
-    <header>
-       <p class="Business">Business Site</p>
-       	  <nav>
-			<a href="index.php">Home</a> |
-			<a href="contact.php">Contact</a> |
-			<a href="browse.php">Browse Products</a>
-		  </nav>	
-    </header>
+<header>
+   <p class="Business">Business Site</p>
+   	  <nav>
+		<a href="index.php">Home</a> |
+		<a href="contact.php">Contact</a> |
+		<a href="browse.php">Browse Products</a>
+	  </nav>	
+</header>
 	
 
